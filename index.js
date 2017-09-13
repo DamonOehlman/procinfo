d8a13fbb936ee45aa67d38b384725961f4526736
Damon Oehlman <damon.oehlman@gmail.com>
2017-09-13 17:16:00 +1000

    formatting cleanup (#5)

--- index.js
+++ index.js
@@ -1,133 +1,133 @@
-var exec = require('child_process').exec,
-    searchers = {},
-    reLineBreak = /\r?\n/,
-    reFloat = /^(\d|\.)+$/,
-    disallowedFields = ['pid', 'comm', 'command', 'args'];
+const exec = require('child_process').exec;
+const searchers = {};
+const reLineBreak = /\r?\n/;
+const reFloat = /^(\d|\.)+$/;
+const disallowedFields = ['pid', 'comm', 'command', 'args'];
 
-var fields = [
-    'state',
-    'ppid',
-    'time',
-    'etime',
-    'cpu',
-    'pcpu',
-    'pmem'
+const fields = [
+  'state',
+  'ppid',
+  'time',
+  'etime',
+  'cpu',
+  'pcpu',
+  'pmem'
 ];
 
 searchers.search = function(pattern, callback) {
-    exec('ps ax -o pid,command', function(err, output) {
-        var lines, processes, results = { pids: [] };
+  exec('ps ax -o pid,command', function(err, output) {
+    const results = { pids: [] };
+    if (err) {
+      return callback(err);
+    }
 
-        if (err) return callback(err);
+    // split the lines on a line break
+    // and remove the header line
+    const lines = output.split(reLineBreak).slice(1);
 
-        // split the lines on a line break
-        // and remove the header line
-        lines = output.split(reLineBreak).slice(1);
-
-        // strip the pid from the lines
-        processes = lines.map(function(line) {
-            return line.slice(6);
-        });
-
-        // iterate through the processes and look for a match against the pattern
-        processes.forEach(function(command, index) {
-            var pid;
-
-            if (pattern.test(command)) {
-                // extract the pid
-                pid = parseInt(lines[index].slice(0, 5), 10);
-
-                // add the pid to the pid array
-                results.pids.push(pid);
-
-                // add the pid detail
-                results[pid] = {
-                    command: command
-                };
-            }
-        });
-
-        // sort the pids
-        results.pids = results.pids.sort();
-
-        // provide the results to the callback
-        callback(null, results);
+    // strip the pid from the lines
+    const processes = lines.map(function(line) {
+      return line.slice(6);
     });
+
+    // iterate through the processes and look for a match against the pattern
+    processes.forEach(function(command, index) {
+      let pid;
+      if (pattern.test(command)) {
+        // extract the pid
+        pid = parseInt(lines[index].slice(0, 5), 10);
+
+        // add the pid to the pid array
+        results.pids.push(pid);
+
+        // add the pid detail
+        results[pid] = {
+          command: command
+        };
+      }
+    });
+
+    // sort the pids
+    results.pids = results.pids.sort();
+
+    // provide the results to the callback
+    callback(null, results);
+  });
 };
 
 searchers.detail = function(pids, callback) {
-    var queryFields = ['pid'].concat(fields).concat(['comm', 'args']),
-        argsFieldIdx = queryFields.length - 1;
+  const queryFields = ['pid'].concat(fields).concat(['comm', 'args']);
+  const argsFieldIdx = queryFields.length - 1;
 
-    exec('ps -o ' + queryFields.join(',') + ' -p' + pids.join(','), function(err, stdout) {
-        var lines, results = { pids: [] };
+  exec('ps -o ' + queryFields.join(',') + ' -p' + pids.join(','), function(err, stdout) {
+    const results = { pids: [] };
+    if (err) {
+      return callback(err);
+    }
 
-        if (err) return callback(err);
+    // iterate through the valid output lines and process the output
+    stdout.split(reLineBreak).slice(1).forEach(function(line) {
+      const fields = line.trim().split(/\s+/);
+      const pid = parseInt(fields[0], 10);
+      const data = {};
 
-        // iterate through the valid output lines and process the output
-        stdout.split(reLineBreak).slice(1).forEach(function(line) {
-            var fields = line.trim().split(/\s+/),
-                pid = parseInt(fields[0], 10),
-                data = {};
+      // if we have a valid pid, then process the line
+      if (pid) {
+        // concat any trailing args into the final arg field
+        fields[argsFieldIdx] = fields.splice(argsFieldIdx + 1);
 
-            // if we have a valid pid, then process the line
-            if (pid) {
-                // concat any trailing args into the final arg field
-                fields[argsFieldIdx] = fields.splice(argsFieldIdx + 1);
-
-                // map the fields onto the object data
-                queryFields.slice(1).forEach(function(fieldName, index) {
-                    var rawValue = fields[index + 1];
-
-                    data[fieldName] = reFloat.test(rawValue) ? parseFloat(rawValue) : rawValue;
-                });
-
-                // calculate the command field
-                data.command = data.comm + ' ' + data.args.join(' ');
-
-                // add the pid data to the results
-                results[pid] = data;
-                results.pids.push(pid);
-            }
+        // map the fields onto the object data
+        queryFields.slice(1).forEach(function(fieldName, index) {
+          const rawValue = fields[index + 1];
+          data[fieldName] = reFloat.test(rawValue) ? parseFloat(rawValue) : rawValue;
         });
 
-        // fire the callback
-        callback(null, results);
+        // calculate the command field
+        data.command = data.comm + ' ' + data.args.join(' ');
+
+        // add the pid data to the results
+        results[pid] = data;
+        results.pids.push(pid);
+      }
     });
+
+    // fire the callback
+    callback(null, results);
+  });
 };
 
 function procinfo(target, callback) {
-    var searchType = 'regex';
+  let searchType = 'regex';
 
-    // if the target is an array or a number then go into pid detail search mode
-    if (Array.isArray(target) || typeof target == 'number') {
-        target = [].concat(target);
-        searchType = 'detail';
-    }
-    else if (typeof target == 'string' || (target instanceof String)) {
-        target = new RegExp(target, 'i');
-    }
+  // if the target is an array or a number then go into pid detail search mode
+  if (Array.isArray(target) || typeof target == 'number') {
+    target = [].concat(target);
+    searchType = 'detail';
+  }
+  else if (typeof target == 'string' || (target instanceof String)) {
+    target = new RegExp(target, 'i');
+  }
 
-    (searchers[searchType] || searchers.search).call(this, target, callback);
+  (searchers[searchType] || searchers.search).call(this, target, callback);
 }
 
 // initialise the fields property on procinfo
 Object.defineProperty(procinfo, 'fields', {
-    get: function() {
-        return [].concat(fields);
-    },
+  get: function() {
+    return [].concat(fields);
+  },
 
-    set: function(newFields) {
-        // reset the fields
-        fields = [];
+  set: function(newFields) {
+    // reset the fields
+    fields = [];
 
-        // iterate through the new fields and add acceptable fields
-        newFields.forEach(function(newField) {
-            if (disallowedFields.indexOf(newField) < 0) {
-                fields[fields.length] = newField;
-            }
-        });
-    }
+    // iterate through the new fields and add acceptable fields
+    newFields.forEach(function(newField) {
+      if (disallowedFields.indexOf(newField) < 0) {
+        fields[fields.length] = newField;
+      }
+    });
+  }
 });
 
 module.exports = procinfo;
--- test/discovery.js
+++ test/discovery.js
@@ -1,13 +1,13 @@
-var assert = require('assert'),
-    procinfo = require('../');
+const assert = require('assert');
+const procinfo = require('../');
 
 describe('process discovery', function() {
-    it('should be able to locate all the node processes', function(done) {
-        procinfo('node', function(err, results) {
-            assert.ifError(err);
-            assert(results.pids.length > 0, 'Could not locate any node processes');
+  it('should be able to locate all the node processes', function(done) {
+    procinfo('node', function(err, results) {
+      assert.ifError(err);
+      assert(results.pids.length > 0, 'Could not locate any node processes');
 
-            done();
-        });
+      done();
     });
+  });
 });
